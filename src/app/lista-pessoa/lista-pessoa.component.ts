6575b08bd273a69c0d5e3edb8f9c154f3d255f76
Marcondes <marcondescurso@outlook.com>
2017-12-04 23:08:18 -0200

    third commit

--- src/app/lista-pessoa/lista-pessoa.component.ts
+++ src/app/lista-pessoa/lista-pessoa.component.ts
@@ -1,22 +1,29 @@
+import { PessoaServiceService } from './pessoa-service.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lista-pessoa',
   templateUrl: './lista-pessoa.component.html',
-  styleUrls: ['./lista-pessoa.component.css']
+  styleUrls: ['./lista-pessoa.component.css'],
+  providers: [PessoaServiceService]
 })
 export class ListaPessoaComponent implements OnInit {
 
-  pessoas: string[] = ['Troll', 'Zezinho', 'Colosso'];
+  pessoas: string[];
   nome: string = "Marcondes";
 
-  constructor() { }
+  constructor(private service: PessoaServiceService) { 
+    this.pessoas = service.getPessoas();
+  }
 
   ngOnInit() {
   }
 
   listar(){
     
+  }
+  enviarNome(){
+    this.service.setPessoa(this.nome);
   }
 
 }
--- src/app/lista-pessoa/pessoa-service.service.ts
+++ src/app/lista-pessoa/pessoa-service.service.ts
@@ -3,6 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class PessoaServiceService {
 
+  nomesPessoas: string [] = ['Troll','Zezim','Colosso','Angular'];
+
   constructor() { }
+
+  getPessoas(): string []{
+    return this.nomesPessoas;
+  }
+
+  setPessoa(nome: string): void{
+    this.nomesPessoas.push(nome);
+  }
 
 }
