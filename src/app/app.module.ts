c48604d66dfb7e93d7451d1422d9c183ce5ba2eb
Marcondes Amaral <marcondescurso@outlook.com>
2017-12-07 17:36:45 -0200

    seventh commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NgIfComponent } from './ng-if/ng-if.component';
 import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgStyleComponent } from './ng-style/ng-style.component';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { NgClassComponent } from './ng-class/ng-class.component';
     NgSwitchCaseComponent,
     NgForComponent,
     NgClassComponent,
+    NgStyleComponent,
   ],
   imports: [
     BrowserModule,
