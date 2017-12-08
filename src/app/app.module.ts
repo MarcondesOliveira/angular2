a9b5cad9581b07c936ab883c3017f4c042cdd3a1
Marcondes <marcondescurso@outlook.com>
2017-12-07 23:47:41 -0200

    eighth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgClassComponent } from './ng-class/ng-class.component';
 import { NgStyleComponent } from './ng-style/ng-style.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 
 @NgModule({
@@ -28,6 +30,8 @@ import { NgStyleComponent } from './ng-style/ng-style.component';
     NgForComponent,
     NgClassComponent,
     NgStyleComponent,
+    NgContentComponent,
+    EventBindingComponent,
   ],
   imports: [
     BrowserModule,
